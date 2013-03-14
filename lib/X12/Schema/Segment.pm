d6c56d3d836afd047c5a265ff1cdb773e1b02ec9
Stefan O'Rear <stefanor@cox.net>
2013-03-13 19:32:51 -0700

    Split Segment from SegmentUse to better match the schema

--- lib/X12/Schema/Segment.pm
+++ lib/X12/Schema/Segment.pm
@@ -3,9 +3,8 @@ package X12::Schema::Segment;
 use Moose;
 use namespace::autoclean;
 
-with 'X12::Schema::Sequencable';
-
 has tag          => (isa => 'Str', is => 'ro', required => 1);
+has friendly     => (isa => 'Str', is => 'ro', required => 1);
 has constraints  => (isa => 'ArrayRef[X12::Schema::Constraint]', is => 'ro', default => sub { [] });
 has elements     => (isa => 'ArrayRef[X12::Schema::Element]', is => 'ro', required => 1);
 has incomplete   => (isa => 'Bool', is => 'ro', default => 0);
@@ -27,7 +26,7 @@ sub encode {
             push @bits, $elem->encode($sink, $value);
         } else {
             if ($elem->required) {
-                die "Segment ".$self->name." element ".$elem->name." is required";
+                die "Segment ".$self->friendly." element ".$elem->name." is required";
             }
 
             push @bits, '';
@@ -38,21 +37,7 @@ sub encode {
     pop @bits while @bits && $bits[-1] eq '';
 
     $sink->segment( join($sink->element_sep, $self->tag, @bits) . $sink->segment_term ) if @bits;
-}
-
-sub BUILD {
-    my ($self) = @_;
-
-    # DIVERSITY: possibly worth restricting use of 'B' type here?
-
-    # This needs a little elaboration.  Yes, we sometimes do not output a value.
-    # But this flag only controls reading, and on read, entirely empty segments
-    # should be suppressed.  If a segment is required, it needs to have at least
-    # one required element.
-    $self->_can_be_empty(0);
-
-    $self->_ambiguous_end_tags({});
-    $self->_initial_tags({ $self->tag => 1 });
+    return @bits ? 1 : 0;
 }
 
 __PACKAGE__->meta->make_immutable;
